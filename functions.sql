aff6082a39c8569ae2fbc0e004f73d891c2b88bb
Atharva Chitale <atharvachtl@gmail.com>
2025-10-26 22:11:49 +0530

    changes

---
+++ functions.sql
@@ -0,0 +1,24 @@
+USE airline_db;
+
+-- 1
+/*
+remove this line and uncomment for FLIGHTS DISPLAY
+
+SELECT * FROM FlightSchedule;
+*/
+
+
+--2
+/*
+remove this line and uncomment to display bookings 
+
+SELECT * FROM Booking;
+*/
+
+
+--3
+/*
+remove this line and uncomment to truncate the database
+
+TRUNCATE TABLE Booking;
+*/
